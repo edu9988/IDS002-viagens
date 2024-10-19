9aca6ac02416a7ab4217b593cdddeb1ec8fcc0ed
edu9988 <edu.jerm@gmail.com>
2024-10-19 18:49:26 -0300

    Create TravelController and TravelService classes

---
+++ src/main/java/com/fatecrl/viagens/controller/TravelController.java
@@ -0,0 +1,69 @@
+package com.fatecrl.viagens.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fatecrl.viagens.model.Travel;
+import com.fatecrl.viagens.service.TravelService;
+
+@RestController
+@RequestMapping("/travels")
+public class TravelController {
+
+    @Autowired
+    private TravelService travelService;
+
+    @GetMapping
+    public ResponseEntity<List<Travel>> getAll(){
+        return ResponseEntity.ok(travelService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Travel> get( @PathVariable("id") Long id ){
+        Travel travel = travelService.find(id);
+
+        if( travel != null )
+            return ResponseEntity.ok(travel);
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Travel> create( @RequestBody Travel travel ){
+        travelService.create( travel );
+        URI uri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand( travel.getId() )
+            .toUri();
+        return ResponseEntity.created( uri ).body( travel );
+    }
+
+    @PutMapping
+    public ResponseEntity<Travel> update( @RequestBody Travel travel ){
+        if( travelService.update(travel) )
+            return ResponseEntity.ok(travel);
+        
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Travel> delete( @PathVariable("id") Long id ) {
+        if( travelService.delete(id) )
+            return ResponseEntity.noContent().build();
+        
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ src/main/java/com/fatecrl/viagens/service/TravelService.java
@@ -0,0 +1,87 @@
+package com.fatecrl.viagens.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.fatecrl.viagens.model.Travel;
+import com.fatecrl.viagens.model.TripType;
+
+@Service
+public class TravelService {
+
+    private static List<Travel> travels = new ArrayList<Travel>();
+
+    public TravelService(){
+        //this is a "fake" travel
+        Travel travel = new Travel();
+        travel.setId(1L);
+        travel.setOrderNumber("12345");
+        travel.setAmount(3);
+        travel.setSource(null);
+        travel.setDestination(null);
+        travel.setStartDateTime( LocalDateTime.now().plusWeeks(1L) );
+        travel.setEndDateTime( LocalDateTime.now().plusWeeks(2L) );
+        travel.setType( TripType.RETURN );
+        travel.setCustomer(null);
+
+        travels.add(travel);
+    }
+
+    public List<Travel> findAll(){
+        return travels;
+    }
+
+    public Travel find( Long id ){
+        return travels.stream()
+            .filter( t -> t.getId() == id )
+            .findFirst().orElse(null);
+    }
+
+    public Travel find( Travel travel ){
+        return travels.stream()
+            .filter( t -> t.equals(travel) )
+            .findFirst().orElse(null);
+    }
+
+    public void create( Travel travel ){
+        Long newId = (long) (travels.size()+1);
+        travel.setId( newId );
+        travels.add( travel );
+    }
+
+    public Boolean update( Travel travel ){
+        Travel _trav = find( travel );
+        if( _trav != null ){
+            if( !travel.getOrderNumber().isBlank() )
+                _trav.setOrderNumber( travel.getOrderNumber() );
+            if( travel.getAmount() != null )
+                _trav.setAmount( travel.getAmount() );
+            if( travel.getSource() != null )
+                _trav.setSource( travel.getSource() );
+            if( travel.getDestination() != null )
+                _trav.setDestination( travel.getDestination() );
+            if( travel.getStartDateTime() != null )
+                _trav.setStartDateTime( travel.getStartDateTime() );
+            if( travel.getEndDateTime() != null )
+                _trav.setEndDateTime( travel.getEndDateTime() );
+            if( travel.getType() != null )
+                _trav.setType( travel.getType() );
+            if( travel.getCustomer() != null )
+                _trav.setCustomer( travel.getCustomer() );
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean delete( Long id ){
+        Travel travel = find(id);
+        if( travel != null ){
+            travels.remove( travel );
+            return true;
+        }
+        return false;
+    }
+}
